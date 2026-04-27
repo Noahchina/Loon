4af7d5beefd5628e721f0963f54e4a725ddae42c
Noah <noah.cn512@gmail.com>
2026-04-27 23:56:26 +0800

    Add files via upload

---
+++ Scripts/Yinpinjianji.js
@@ -0,0 +1,33 @@
+/******************************
+脚本功能：音频剪辑-登陆后解锁会员
+脚本作者：afengye
+脚本频道：https://t.me/afengye
+更新时间：2024-08-06
+使用声明：️基于原作者修改使可用于Loon
+*******************************
+[Rewrite]
+^https:\/\/ad\.camoryapps\.com\/(\.?)+ - reject-200
+
+[Script]
+http-response ^https:\/\/pay\.camoryapps\.com\/.* script-path= https://raw.githubusercontent.com/Noahchina/Loon/main/Scripts/Yinpinjianji.js, requires-body=true, timeout=60, tag=Yinpinjianji
+
+
+[MITM]
+hostname = pay.camoryapps.com, ad.camoryapps.com
+*******************************/
+
+var austin = $response.body;
+var obj = JSON.parse(austin);
+
+// 强制会员
+if (obj.data) {
+    obj.data.isR = true;
+    obj.data.isSubscribe = true;
+    obj.data.timeExpire = "2999-01-01 00:00:00";
+
+    obj.data.vip = true;
+    obj.data.isVip = true;
+    obj.data.vipType = 1;
+}
+
+$done({ austin: JSON.stringify(obj) });
